f1c4ba4084b12ada0d2f54889130a94536e01e88
Rei Hakurei <reimu_after_marisa@yahoo.com>
2019-12-10 20:59:50 +0700

    hotfix2

--- app/models/tahun_ajaran.rb
+++ app/models/tahun_ajaran.rb
@@ -7,5 +7,5 @@ class TahunAjaran < ApplicationRecord
     self.id = Integer("%04d%02d" % [self.tahun, self.semester],10)
   end
   validates :tahun, numericality: { only_integer: true }, length: { is: 4 }
-  validates :semester, numericality: { only_integer: true }, length: { is: 2 }
+  validates :semester, numericality: { only_integer: true }, length: { in: 1..2 }
 end
